0a34f793536e51f68732466cfb216dfd4dadb992
Hong min ki <gaogai2@naver.com>
2023-07-19 19:59:25 +0900

    add 별찍기-10.py

---
+++ 백준/단계별풀어보기/21.재귀/별찍기-10.py
@@ -0,0 +1,21 @@
+def star(l):
+    if l == 3:
+        return ['***', '* *', '***']
+
+    arr = star(l // 3)
+    stars = []
+
+    for i in arr:
+        stars.append(i * 3)
+
+    for i in arr:
+        stars.append(i + ' ' * (l // 3) + i)
+
+    for i in arr:
+        stars.append(i * 3)
+
+    return stars
+
+
+n = int(input())
+print('\n'.join(star(n)))
