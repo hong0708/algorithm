76ee4a606fd8afdff136b147a7a85f7eaa4df2fb
Hong min ki <gaogai2@naver.com>
2023-07-21 23:32:44 +0900

    add 가장긴증가하는부분수열.py

---
+++ 백준/단계별풀어보기/23.동적계획법/가장긴증가하는부분수열.py
@@ -0,0 +1,8 @@
+n = int(input())
+arr = list(map(int, input().split()))
+ans = [1 for _ in range(n)]
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            ans[i] = max(ans[i], ans[j] + 1)
+print(max(ans))
