3e09cd7109bb3b239321bbc89f14778494feb6d3
Hong min ki <gaogai2@naver.com>
2023-10-10 13:46:27 +0900

    fix 음악프로그램.py

--- 백준/solved.ac/5단계/음악프로그램.py
+++ 백준/solved.ac/5단계/음악프로그램.py
@@ -1,38 +1,35 @@
+# https://www.acmicpc.net/problem/2623
+
+import sys
 from collections import deque
 
 n, m = map(int, input().split())
-indegree = [0] * (n + 1)
-graph = [[] for _ in range(n + 1)]
+arr = [0 for _ in range(n + 1)]
+r = [[] for _ in range(n + 1)]
+ans = []
 
 for _ in range(m):
-    arr = list(map(int, input().split()))
-    for i in range(1, len(arr) - 1):
-        graph[arr[i]].append(arr[i + 1])
-        indegree[arr[i + 1]] += 1
+    impl = list(map(int, sys.stdin.readline().split()))
+    dg = 1
+    for i in range(2, len(impl)):
+        arr[impl[i]] += 1
+        dg += 1
+        r[impl[i - 1]].append(impl[i])
 
+dq = deque()
+for i in range(1, n + 1):
+    if arr[i] == 0:
+        dq.append(i)
 
-# 위상 정렬 함수
-def topology_sort():
-    q = deque()
-    result = []
-
-    for i in range(1, n + 1):
-        if indegree[i] == 0:
-            q.append(i)
-
-    while q:
-        now = q.popleft()
-        result.append(now)
-
-        for i in graph[now]:
-            indegree[i] -= 1
-            if indegree[i] == 0:
-                q.append(i)
-
-    if len(result) != n:
-        print(0)
-    else:
-        for singer in result:
-            print(singer)
-
-topology_sort()
+while dq:
+    now = dq.popleft()
+    ans.append(now)
+    for i in r[now]:
+        arr[i] -= 1
+        if arr[i] == 0:
+            dq.append(i)
+if len(ans) == n:
+    for i in ans:
+        print(i)
+else:
+    print(0)
