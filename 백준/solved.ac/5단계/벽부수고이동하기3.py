635e0569a586c46783697fbe8523fc12533fc387
Hong min ki <gaogai2@naver.com>
2023-10-15 00:09:32 +0900

    fix 벽부수고이동하기3.py

--- 백준/solved.ac/5단계/벽부수고이동하기3.py
+++ 백준/solved.ac/5단계/벽부수고이동하기3.py
@@ -7,28 +7,32 @@ dy = [0, 0, -1, 1]
 
 
 def mapping():
-    visited = [[[n * m] * (k + 1) for _ in range(m)] for _ in range(n)]
-    visited[0][0][k] = 1
+    visited = [[[False] * (k + 1) for _ in range(m)] for _ in range(n)]
+    visited[0][0][k] = True
 
     q = deque()
-    q.append([0, 0, k, True])
+    q.append([0, 0, k, True, 1])
     while q:
-        x, y, c = q.popleft()  # 좌표와 벽을 부술 수 있는 남은 횟수
+        x, y, c, day, ans = q.popleft()  # 좌표와 벽을 부술 수 있는 남은 횟수
         if x == n - 1 and y == m - 1:
-            return visited[x][y][c]
+            return ans
         for i in range(4):
             tx = dx[i] + x
             ty = dy[i] + y
 
-            if 0 <= tx < n and 0 <= ty < m:
-                if mapp[tx][ty] == '1' and c > 0 and visited[x][y][c] + 1 < visited[tx][ty][c - 1]:  # 벽을 한 개 부숨
-                    visited[tx][ty][c - 1] = visited[x][y][c] + 1
-                    q.append([tx, ty, c - 1])
-                    # c를 -1해서 저장
+            if 0 <= tx < n and 0 <= ty < m and not visited[tx][ty][c]:
                 # 빈 방이라면
-                elif mapp[tx][ty] == '0' and visited[x][y][c] + 1 < visited[tx][ty][c]:
-                    visited[tx][ty][c] = visited[x][y][c] + 1
-                    q.append([tx, ty, c])
+                if mapp[tx][ty] == '0':
+                    visited[tx][ty][c] = True
+                    q.append([tx, ty, c, not day, ans + 1])
+
+                if mapp[tx][ty] == '1' and c > 0:  # 벽을 한 개 부숨
+                    if day:
+                        visited[tx][ty][c - 1] = True
+                        q.append([tx, ty, c - 1, not day, ans + 1])
+                    # c를 -1해서 저장
+                    else:
+                        q.append([x, y, c, not day, ans + 1])
     return -1
 
 
