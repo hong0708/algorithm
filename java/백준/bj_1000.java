38fd5cde70a6c3c71fbe3aae56028a6732ad7e57
Hong min ki <gaogai2@naver.com>
2023-10-16 23:38:53 +0900

    add bj_1000.java

---
+++ java/백준/bj_1000.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_1000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String string = br.readLine();
+        StringTokenizer st = new StringTokenizer(string, " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a + b);
+
+    }
+}
