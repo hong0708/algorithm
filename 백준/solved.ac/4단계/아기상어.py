c42be2edfa9eab1b3f6e7364c95ae1fc94e57809
Hong min ki <gaogai2@naver.com>
2023-10-25 01:25:26 +0900

    fix 아기상어.py

--- 백준/solved.ac/4단계/아기상어.py
+++ 백준/solved.ac/4단계/아기상어.py
@@ -11,55 +11,45 @@ n = int(sys.stdin.readline().rstrip())
 arr = []
 
 for i in range(n):
-    impl = list(map(int, sys.stdin.readline().split()))
-    if 9 in impl:
-        a, b = i, impl.index(9)
-        impl[b] = 0
-    arr.append(impl)
+    impls = list(map(int, sys.stdin.readline().split()))
+    if 9 in impls:
+        k, l = i, impls.index(9)
+        impls[l] = 0
+    arr.append(impls)
 
-eat = 0
-baby = 2
-time = 0
-end = True
-while end:
-    call = True
-    loc = deque()
-    loc.append([a, b, baby, eat, time])
 
-    # 현재 위치
-    while loc:
-        now_loc = loc.popleft()
-        # 현재 위치 기준으로 다음 갈 장소 탐색
-        dq = deque()
-        dq.append([now_loc[0], now_loc[1], now_loc[2], now_loc[3], now_loc[4]])
+def next_loc(x, y):
+    visited = [[False for _ in range(n)] for _ in range(n)]
+    dq = deque()
+    dq.append([x, y, 0])
+    impl = []
+    visited[x][y] = True
+    while dq:
+        a, b, c = dq.popleft()
+        for j in range(4):
+            nx, ny = a + dx[j], b + dy[j]
+            if -1 < nx < n and -1 < ny < n and not visited[nx][ny] and arr[nx][ny] <= bs:
+                # 먹을 수 있는 물고기 있는 곳이라면
+                if 0 < arr[nx][ny] < bs:
+                    impl.append([c + 1, nx, ny])
+                # 먹을 수 없거나 그냥 길이라면 그냥 간다.
+                else:
+                    dq.append([nx, ny, c + 1])
+                visited[nx][ny] = True
+    return sorted(impl, key=lambda x: (x[0], x[1], x[2]))
 
-        visited = [[False for _ in range(n)] for _ in range(n)]
-        call = True
-        while call and dq:
-            now = dq.popleft()
-            x, y, bs, e, t = now[0], now[1], now[2], now[3], now[4]
-            call = True
-            # 먼저 최대한 위 그리고 왼쪽을 본다 먹이 우선 순위 때문에
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                # 길을 갈 수 있으면 간다
-                if -1 < nx < n and -1 < ny < n and not visited[nx][ny] and arr[nx][ny] <= bs:
-                    # 먹을 수 있는 물고기 있는 곳이라면
-                    if 0 < arr[nx][ny] < bs:
-                        if e + 1 == bs:
-                            loc.append([nx, ny, bs + 1, 0, t + 1])
-                        else:
-                            loc.append([nx, ny, bs, e + 1, t + 1])
-                        arr[nx][ny] = 0
-                        call = False
-                        break
-                    # 먹을 수 없거나 그냥 길이라면 그냥 간다.
-                    else:
-                        dq.append([nx, ny, bs, e, t + 1])
-                        visited[nx][ny] = True
 
-        # 다음 길들을 확인했는데 더이상 먹을 수 있는 케이스가 없음
-        if call:
-            print(now_loc[4])
-            end = False
+loc = deque()
+loc.append([k, l, 2, 0, 0])
+while loc:
+    loc_x, loc_y, bs, e, t = loc.popleft()
+    lis = next_loc(loc_x, loc_y)
+    if len(lis) == 0:
+        print(t)
+        break
+    nt, n_x, n_y = lis[0]
+    arr[n_x][n_y] = 0
+    if e + 1 == bs:
+        loc.append([n_x, n_y, bs + 1, 0, t + nt])
+    else:
+        loc.append([n_x, n_y, bs, e + 1, t + nt])
