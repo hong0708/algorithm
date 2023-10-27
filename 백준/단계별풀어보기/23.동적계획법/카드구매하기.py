cd62a769e43e5736f911fc65ae33c1ba2dbd2615
hong0708 <gaogai2@naver.com>
2023-10-28 00:06:01 +0900

    fix 카드구매하기.py

--- 백준/단계별풀어보기/23.동적계획법/카드구매하기.py
+++ 백준/단계별풀어보기/23.동적계획법/카드구매하기.py
@@ -1,9 +1,11 @@
-N = int(input())
-p = [0] + list(map(int,input().split()))
-dp = [0 for _ in range(N+1)]
+# https://www.acmicpc.net/problem/11052
 
+# n개의 카드 구매를 위한 n 종류의 카트팩 종류는 개수를 표현한다
+n = int(input())
+p = [0] + list(map(int, input().split()))
+dp = [0 for _ in range(n + 1)]
 
-for i in range(1,N+1):
-    for k in range(1,i+1):
-        dp[i] = max(dp[i], dp[i-k] + p[k])
-print(dp[i])
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + p[j])
+print(dp[n])
